9e97dc225aba79ff67353106918eab237b4864f5
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-02-26 15:35:35 -0500

    Feature: Login

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { UserAuth } from "../context/AuthContext";
 
 export function MyRoutes() {
+  const { user } = UserAuth();
+  const RequireAuth = ({children})=>{
+    return user?children:<Navigate to={"/login"}/>
+  }
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<RequireAuth>
+          <Home />
+        </RequireAuth>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
